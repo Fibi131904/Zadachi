3a417f478f28f7b8d4cc214db06821bd82bea94b
Kseniya Smirnova <fanni@bk.ru>
2022-01-24 12:44:44 +0300

    первый переданный аргумент зканчивается   вторым аргументом

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function hexToDec(hexString){
-  return  parseInt(hexString, 16);
- }
+function solution(str, ending){
+  return str.endsWith(ending);
+}
+/* 
+Завершите решение так, чтобы оно возвращало true,
+ если первый переданный аргумент (строка) заканчивается
+  вторым аргументом (тоже строкой)*/
