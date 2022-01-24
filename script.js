2a61c7ba444331f1a0a27ee2d6426320b6bf9c12
Kseniya Smirnova <fanni@bk.ru>
2022-01-24 11:12:42 +0300

    преобразjdfybt шестнадцатеричное числа  в десятичное число.

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-const winsOver = {
-  "rock" : "scissors",
-  "scissors" : "paper",
-  "paper" : "rock"
-};
-
-const rps = (p1, p2) => {
-if (p1 === p2)
-  return "Draw!";
-else if (winsOver[p1] === p2)
-  return "Player 1 won!";
-else
-  return "Player 2 won!";  
-};
-/* Камень ножницы Бумага
-Давайте играть! Вы должны вернуть, какой игрок выиграл! В случае ничьей возврат Draw!.
-
-Примеры:
-
-rps('scissors','paper') // Player 1 won!
-rps('scissors','rock') // Player 2 won!
-rps('paper','paper') // Draw! 
-*/
+function hexToDec(hexString){
+  return  parseInt(hexString, 16);
+ }
