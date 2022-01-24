a367be57ed8b641ab60266f221acc93b093ec92f
Kseniya Smirnova <fanni@bk.ru>
2022-01-24 10:47:37 +0300

    Сравнения с логическим оператором Or( 1 из оперантов равен)

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-function testGreaterThan(val) {
-  if (val >100 ) {  
-    return "Over 100";
-  }
+function testLogicalOr(val) {
 
-  if (val > 10) { 
-    return "Over 10";
+  if (val < 10 || val > 20) {
+    return "Outside";
   }
+  return "Inside";
 
-  return "10 or Under";
 }
 
-testGreaterThan(10);
+testLogicalOr(15);
+
+/*
+if (num > 10) {
+  return "No";
+}
+if (num < 5) {
+  return "No";
+}
+return "Yes";
+
+Та же логика может быть записана как:
+
+if (num > 10 || num < 5) {
+  return "No";
+}
+return "Yes"; */
