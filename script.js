2adbe100c0c1fd41150bbb21b20041efc474d2ee
Kseniya Smirnova <fanni@bk.ru>
2022-01-24 10:19:21 +0300

    Сравнение с оператором больше, чем

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-function compareEquality(a, b) {
-  if (a === b) { 
-    return "Equal";
+function testGreaterThan(val) {
+  if (val >100 ) {  
+    return "Over 100";
   }
-  return "Not Equal";
+
+  if (val > 10) { 
+    return "Over 10";
+  }
+
+  return "10 or Under";
 }
 
-compareEquality(10, "10");
-//Примечание. В JavaScript вы можете определить тип переменной или значения с помощью typeofоператора следующим образом:
-
-typeof 3
-typeof '3'
+testGreaterThan(10);
