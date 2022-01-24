9558def0df6b0759ba5e25dde3c0992226eb3c60
Kseniya Smirnova <fanni@bk.ru>
2022-01-24 10:13:17 +0300

    Оператор строгого равенства ===

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function testEqual(val) {
-  if (val==12) { 
+function compareEquality(a, b) {
+  if (a === b) { 
     return "Equal";
   }
   return "Not Equal";
 }
 
-testEqual(10);
+compareEquality(10, "10");
+//Примечание. В JavaScript вы можете определить тип переменной или значения с помощью typeofоператора следующим образом:
+
+typeof 3
+typeof '3'
