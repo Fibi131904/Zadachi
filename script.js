2ab1b1cc8563f4cce4c034a03dec041d9500cc7d
Kseniya Smirnova <fanni@bk.ru>
2022-01-25 11:33:37 +0300

    несколько условий if,  else if,  else

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-function testElse(val) {
-  let result = "";
+function testElseIf(val) {
+  if (val > 10) {  // если верно условие это, верни..
+    return "Greater than 10"; // это
+  } else if (val < 5) {// если это условие верно, верни
+    return "Smaller than 5"; // это
+  } else { // если условия выше не верны, верни
 
-
-  if (val > 5) {     // Если val больше 5, то 
-    result = "Bigger than 5";
-  } else {  // Иначе
-    result = "5 or Smaller";
+  return "Between 5 and 10"; // это
   }
-  
-  return result;
 }
-
-testElse(4); //вызов с параметром 4
+testElseIf(7);
