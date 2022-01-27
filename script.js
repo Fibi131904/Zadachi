f17e91f67669cbb6166b3388a62cc86b9a93d4fe
Kseniya Smirnova <fanni@bk.ru>
2022-01-27 14:59:14 +0300

    Добавление в массив, поиск среднего, сравнение

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function accum(s) {
-  return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
+function betterThanAverage(classPoints, yourPoints) {
+  // Your code here
+  var classAvg = 0;
+  for (var i = 0; i < classPoints.length; i++){
+    classAvg += classPoints[i]; 
+  }
+  classAvg = classAvg/classPoints.length; 
+  return yourPoints > classAvg;
 }
+
+/*дан массив. добавить свое число. посчитайть среднее и сравнить со своим числом.
+
+Вернуть True, если твое больше, иначе False
